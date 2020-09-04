5768efba21bf1df7d3c8dd2259ea05559f21c845
EugenioDiPaola <68637309+EugenioDiPaola@users.noreply.github.com>
2020-09-04 22:14:20 +0200

    Update Capitolo7.hs

--- Capitolo7.hs
+++ Capitolo7.hs
@@ -3,7 +3,7 @@
 
 -- [f x | x <- xs , p x] può essere scritta come un funzione g siffatta:
 
-g f p = map f . filter p
+g f p = map f (filter p xs)
 
 
 -- 2. Without looking at the definitions from the standard prelude, define the following higher-order library functions on lists.
@@ -45,8 +45,16 @@ any :: (a -> Bool) -> [a] -> Bool
 any p xs = foldr ((||).p) False xs
 
 -- c.
-takeWhile :: 
-
+takeWhile :: (a -> Bool) - > [a] -> [a]
+takeWhile p [] = []
+takeWhile | p x = x : takeWhile p xs
+          | not (p x) = []
+	  
+-- d.
+dropWhile :: (a -> Bool) - > [a] -> [a]
+dropWhile p [] = []
+dropWhile | p (x:xs) = dropWhile p xs
+          | not (x:xs) = xs
 
 -- Le funzioni foldr e foldl sono così definiti nello standard prelude:
 
@@ -72,26 +80,33 @@ map f (x : xs) = f x : map f xs
 map :: (a -> b) -> [a] -> [b]
 map f xs = foldr (f.(:).f) (:[]) xs
 
+filter :: (a -> Bool) -> [a] -> [a]
+filter 
+
 -- 4. Using foldl, define a function dec2int :: [Int] -> Int that converts a
 -- decimal number into an integer. For example:
 -- > dec2int [2,3,4,5]
 -- 2345
 
+
+
 -- 5. Without looking at the definitions from the standard prelude, define the
--- higher-order library function c u r r y that converts a function on pairs into
--- a curried function, and, conversely, the function u n c u r r y that converts a curried function with two arguments into a function on pairs.
+-- higher-order library function curry that converts a function on pairs into
+-- a curried function, and, conversely, the function uncurry that converts a 
+-- curried function with two arguments into a function on pairs.
 -- Hint: first write down the types of the two functions.
 
 
--- 6. A higher-order function unfold that encapsulates a simple pattern of recursion for producing a list can be defined as follows:
+-- 6. A higher-order function unfold that encapsulates a simple pattern of recursion 
+-- for producing a list can be defined as follows:
 -- unfold p h t x | p x = [ ]
 --                | otherwise = h x : unfold p h t ( t x )
--- That is, the function u n f o l d p h t produces the empty list if the predicate p
+-- That is, the function unfold p h t produces the empty list if the predicate p
 -- is true of the argument value, and otherwise produces a non-empty list by
 -- applying the function h to this value to give the head, and the function t
 -- to generate another argument that is recursively processed in the same way
 -- to produce the tail of the list. For example, the function int2bin can be
--- rewritten more compactly using u n f o l d as follows:
+-- rewritten more compactly using unfold as follows:
 -- int2bin = unfold (== 0) (‘mod‘ 2) (‘div‘ 2)
 -- Redefine the functions chop8, m a p f and iterate f using unfold.
 
@@ -109,17 +124,18 @@ map f xs = foldr (f.(:).f) (:[]) xs
 
 
 -- 8. Test your new string transmitter program from the previous exercise using a
--- faulty communication channel that forgets the first bit, which can be modelled using the t a i l function on lists of bits.
+-- faulty communication channel that forgets the first bit, which can be modelled 
+-- using the tail function on lists of bits.
 
 
--- 9. Define a function altMap :: (a->b) -> (a->b) -> [a] -> [b] that
+-- 9. Define a function altMap :: (a -> b) -> (a -> b) -> [a] -> [b] that
 -- alternately applies its two argument functions to successive elements in a list,
 -- in turn about order. For example:
--- > altMap (+10) (+100) [0,1,2,3,4]
--- [10,101,12,103,14]
+-- > altMap (+10) (+100) [0, 1, 2, 3, 4]
+-- [10, 101, 12, 103, 14]
 
 
--- 10. Using a l t M a p, define a function l u h n : : [ I n t ] - > B o o l that implements
+-- 10. Using altMap, define a function luhn :: [Int] -> Bool that implements
 -- the Luhn algorithm from the exercises in chapter 4 for bank card numbers of
 -- any length. Test your new function using your own bank card.
 
