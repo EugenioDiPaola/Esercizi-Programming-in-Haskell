498a1fbda48e0699f01a4d912d90cc513030adc7
EugenioDiPaola <68637309+EugenioDiPaola@users.noreply.github.com>
2020-09-05 17:00:37 +0200

    Update Capitolo 3 - Esercizi.hs

--- Capitolo 3 - Esercizi.hs
+++ Capitolo 3 - Esercizi.hs
@@ -7,11 +7,16 @@
 -- ([False, True],[’0’, ’1’] )
 -- [tail, init, reverse]
 
-:type ['a', 'b', 'c']
---
+-- ['a', 'b', 'c'] è di tipo Char
 
+-- ('a', 'b', 'c') è di tipo (Char, Char, Char)
 
-[tail, init, reverse] :: [[a] -> [a]]
+-- [(False, 'O'), (True, '1')] è di tipo [(Bool, Char)]
+
+-- ([False, True], ['0', '1']) è di tipo ([Bool], [Char])
+
+-- [tail, init, reverse] è di tipo [[a] -> [a]]
+
 
 -- 2. Write down definitions that have the following types; it does not matter what
 -- the definitions actually do as long as they are type correct.
@@ -31,18 +36,39 @@ copy = \x -> (x, x)
 
 apply = \f x -> f x
 
+
 -- 3. What are the types of the following functions?
--- secondxs=head(tailxs)
--- swap(x,y)=(y,x)
--- pairxy=(x,y)
--- doublex=x*2
+-- second xs = head (tail xs)
+-- swap (x, y) = (y, x)
+-- pair x y = (x, y)
+-- double x = x * 2
 -- palindrome xs = reverse xs == xs
 -- twice f x = f (f x)
 -- Hint: take care to include the necessary class constraints in the types if the
 -- functions are defined using overloaded operators.
 
+second :: [a] -> a
+
+swap :: (a, b) -> (b, a)
+
+palindrome :: Eq a => [a] -> Bool
+
+double :: Num a => a -> a
+
+pair :: a -> b -> (a, b)
+
+twice :: (a -> a) -> a -> a
+
+
 -- 4. Check your answers to the preceding three questions using GHCi.
 
--- 5. Why is it not feasible in general for function types to be instances of the E q
+-- Basta usare :type seguito dall'espressione.
+
+
+-- 5. Why is it not feasible in general for function types to be instances of the Eq
 -- class? When is it feasible? Hint: two functions of the same type are equal if
 -- they always return equal results for equal arguments.
+
+-- Perché spesso le funzioni hanno un numero di argomenti infinito o molto grande.
+
+
