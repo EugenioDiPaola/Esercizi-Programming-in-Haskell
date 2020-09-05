59f7663897b76965ca7fca3f8a0f618e1ec79f6c
EugenioDiPaola <68637309+EugenioDiPaola@users.noreply.github.com>
2020-09-05 14:34:48 +0200

    Update CaesarCipher.hs

--- CaesarCipher.hs
+++ CaesarCipher.hs
@@ -1,19 +1,19 @@
-import Data.Char
+import Data.Char;
 
 convertCharToInt :: Char -> Int
 convertCharToInt c | isLower c = [ i | (i, t) <- zip [0..25] ['a'..'z'], t == c] !! 0
                    | isUpper c = [ i | (i, t) <- zip [26..51] ['A'..'Z'], t == c] !! 0
 
-convertCharToInt :: Char -> Int
-convertCharToInt c | isLower c = length['a'..c] - 1
+-- convertCharToInt :: Char -> Int
+-- convertCharToInt c | isLower c = length['a'..c] - 1
                    | isUpper c = 26 + length['A'..c] - 1
 
 convertIntToChar :: Int -> Char
 convertIntToChar i | i >= 0 && i <= 25 = [ c | (c, t) <- zip ['a'..'z'] [0..25], t == i] !! 0
                    | i >= 26 && i <= 51 = [ c | (c, t) <- zip ['A'..'Z'] [26..51], t == i] !! 0
 
-convertIntToChar :: Int -> Char
-convertIntToChar i =  (['a'..'z'] ++ ['A'..'Z']) !! i
+-- convertIntToChar :: Int -> Char
+-- convertIntToChar i =  (['a'..'z'] ++ ['A'..'Z']) !! i
 
 caesarCypherChar :: Char -> Int -> Char
 caesarCypherChar c d | isLower c = convertIntToChar (mod (convertCharToInt c + mod d 26) 26)
