41a63edbba4d366794c719d6cdf0df0482a3c0d2
EugenioDiPaola <68637309+EugenioDiPaola@users.noreply.github.com>
2020-09-06 19:24:51 +0200

    Update Capitolo 3 - Esercizi.hs

--- Capitolo 3 - Esercizi.hs
+++ Capitolo 3 - Esercizi.hs
@@ -1,4 +1,46 @@
--- CAPITOLO 3 - TYPES AND CLASSES
+-- CAPITOLO 1 - INTRODUCTION - Programming with Haskell - Graham Hutton - II Edizione (2016)
+
+-- 1. Give another possible calculation for the result of double (double 2).
+
+-- double (double 2)
+-- = double (2 + 2)
+-- = (2 + 2) + (2 + 2)
+-- = 4 + (2 + 2)
+-- = 4 + 4
+-- = 8
+
+
+-- 2. Show that sum [x] = x for any number x.
+
+-- sum [x ]
+-- = x + sum [ ]
+-- = x + 0
+-- = x
+
+
+-- 3. Define a function product that produces the product of a list of numbers,
+-- and show using your definition that product [2, 3, 4 ] = 24.
+
+product :: [Int] -> Int
+product [] = 1
+product x:xs = x * product xs
+
+-- product [2, 3, 4]
+-- = 2 ∗ (product [3, 4])
+-- = 2 ∗ (3 ∗ product [4])
+-- = 2 ∗ (3 ∗ (4 ∗ product []))
+-- = 2 ∗ (3 ∗ (4 ∗ 1))
+-- = 24
+
+
+-- 4. How should the definition of the function qsort be modified so that it produces a reverse sorted version of a list?
+
+
+
+-- 5. What would be the effect of replacing < = by < in the original definition of
+-- qsort? Hint: consider the example qsort [2, 2, 3, 1, 1].
+
+-- CAPITOLO 3 - INTRODUCTION - Programming with Haskell - Graham Hutton - II Edizione (2016)
 
 -- 1. What are the types of the following values?
 -- ['a', 'b', 'c']
