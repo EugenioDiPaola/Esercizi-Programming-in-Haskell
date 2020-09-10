3bc5423d818c4dd0aa8a0467a4d3184a17deffc9
EugenioDiPaola <68637309+EugenioDiPaola@users.noreply.github.com>
2020-09-10 21:19:54 +0200

    Update and rename Capitolo 3 - Esercizi.hs to Capitolo 1-3 - Esercizi.hs

--- Capitolo 1-3 - Esercizi.hs
+++ Capitolo 1-3 - Esercizi.hs
@@ -12,8 +12,8 @@
 
 -- 2. Show that sum [x] = x for any number x.
 
--- sum [x ]
--- = x + sum [ ]
+-- sum [x]
+-- = x + sum []
 -- = x + 0
 -- = x
 
@@ -35,10 +35,50 @@ product x:xs = x * product xs
 
 -- 4. How should the definition of the function qsort be modified so that it produces a reverse sorted version of a list?
 
-
+qsortReverse :: Ord a => [a] -> [a]
+qsortReverse [] = []
+qsortReverse (x:xs) = qsortReverse smaller ++ [x] ++ qsortReverse larger
+               where
+                  smaller = [a | a <- xs, a * x ]
+                  larger = [b | b <- xs, b > x ]
 
 -- 5. What would be the effect of replacing < = by < in the original definition of
 -- qsort? Hint: consider the example qsort [2, 2, 3, 1, 1].
+
+-- Rimuove i duplicati, cioè qsortReverse [2, 2, 3, 1, 1] con <= invece che < restituisce [1, 2, 3] invece che [1, 1, 2, 2, 3].
+
+
+
+-- CAPITOLO 2 - FIRST STEPS - Programming with Haskell - Graham Hutton - II Edizione (2016)
+
+-- 1. Parenthesise the following arithmetic expressions:
+-- 2 ^ 3 ∗ 4
+-- 2 ∗ 3 + 4 ∗ 5
+-- 2 + 3 ∗ 4 ^ 5
+
+-- (2 ^ 3) * 4
+-- (2 * 3) + (4 * 5)
+-- 2 + (3 * (4 * 5))
+
+
+-- 2. Work through the examples from this chapter using Hugs.
+
+
+-- 3. The script below contains three syntactic errors. Correct these errors
+-- and then check that your script works properly using Hugs.
+-- N = a ’div’ length xs
+-- where
+-- a = 10
+-- xs = [1, 2, 3, 4, 5]
+
+
+-- 4. Show how the library function last that selects the last element of a nonempty list could be defined in terms of the library functions introduced
+-- in this chapter. Can you think of another possible definition?
+
+
+-- 5. Show how the library function init that removes the last element from
+-- a non-empty list could similarly be defined in two different ways.
+
 
 -- CAPITOLO 3 - INTRODUCTION - Programming with Haskell - Graham Hutton - II Edizione (2016)
 
